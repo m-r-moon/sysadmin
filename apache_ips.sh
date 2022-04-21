c2ad1210a9401902f867b3595ce3bb481d43b135
Mr Moon <mrmoon@una.edu>
2022-04-21 10:20:05 -0500

    removed if statement
    revised the script to include the url in addition to the ip address. additional updates required to improve accuracy.

--- apache_ips.sh
+++ apache_ips.sh
@@ -3,7 +3,7 @@
 if [ "$#" -ne "1" ]; then
   echo "Usage: apache_ips.sh APACHE_LOG_FILE_PATH"$'\n'
 else
-  awk -F] '{ if ($NF = 5) split($4, a, " "); split(a[2], ar, ":"); print ar[1] }' $1
+  awk -F] '{ split($4, a, " "); split(a[2], ar, ":"); split($5, d, ":"); print ar[1], length(d[4] > 1) ? d[4]":"d[5] : "" }' $1
   
   echo "Done."$'\n'
 fi
